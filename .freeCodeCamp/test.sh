f2a4dbeaee26505074a71202e6a3bc1bce92050b
Kareem Hatem <kareem.hatem013@gmail.com>
2025-04-01 08:54:09 +0000

    Save progress: 950.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -2,7 +2,9 @@
 # Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
 I=1
-NUMBER=$(( RANDOM % 75  + 1))
+NUMBER=$(( RANDOM % 15  + 1))
 TEXT="The next number is, "
-echo "The next number is B:"$NUMBER
-echo $TEXT
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+fi
--- bingo.sh
+++ bingo.sh
@@ -4,4 +4,7 @@ echo -e "\n~~ Bingo Number Generator ~~\n"
 I=1
 NUMBER=$(( RANDOM % 75  + 1))
 TEXT="The next number is, "
-echo $NUMBER
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+fi
